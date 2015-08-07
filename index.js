1325f495d5e43407b4c8c1b7e5683d776777c165
Tenor Biel <tenorbiel@gmail.com>
2015-08-07 15:27:18 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(factory);
+  } else if (typeof exports === 'object') {
+    factory(exports);
+  } else {
+    root.FizzBuzz = {};
+
+    factory(root.FizzBuzz);
+  }
+})(this, function() {
+  exports.combinations = [
+    {number: 3, word: 'Fizz'},
+    {number: 5, word: 'Buzz'}
+  ];
+
+  exports.wordFor = wordFor;
+  exports.run = run;
+
+  if (exports.main === module) {
+    exports.run();
+  }
+
+  function wordFor(number) {
+    var result = '';
+
+    exports.combinations.forEach(function(combination) {
+      if (number % combination.number === 0) {
+        result += combination.word;
+      }
+    });
+
+    return result || number.toString();
+  }
+
+  function run() {
+    for (var i = 1; i <= 100; i++) {
+      console.log(findWord(i));
+    }
+  }
+});
---
+++ test/fizzbuzz.js
@@ -0,0 +1,43 @@
+
+
+
+var expect = require('expect');
+
+var fizzbuzz = require('../');
+
+describe('FizzBuzz', function() {
+  describe('.wordFor()', function() {
+    it('returns "Buzz" for numbers divisible by just 5', function() {
+      expect(fizzbuzz.wordFor(25)).toBe('Buzz');
+    });
+
+    it('returns "Fizz" for numbers divisible by just 3', function() {
+      expect(fizzbuzz.wordFor(72)).toBe('Fizz');
+    });
+
+    it('returns the number for numbers not divisble by either', function() {
+      expect(fizzbuzz.wordFor(7)).toBe('7');
+    });
+
+    context('when customized', function() {
+      before(function() {
+        fizzbuzz.combinations.push({
+          number: 7,
+          word: 'Dog'
+        });
+      });
+
+      after(function() {
+        fizzbuzz.combinations.pop();
+      });
+
+      it('returns "Dog" for numbers divisible by just 7', function() {
+        expect(fizzbuzz.wordFor(7)).toBe('Dog');
+      });
+
+      it('returns "FizzDog" for numbers divisible by 3 and 7', function() {
+        expect(fizzbuzz.wordFor(21)).toBe('FizzDog');
+      });
+    });
+  });
+});
